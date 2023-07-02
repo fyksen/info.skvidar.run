663160b362cd0929864d6431f18392fb0b843d23
Fredrik Fyksen <fredrik@fyksen.me>
2023-07-02 23:55:30 +0200

    Added plausible tracking to config. Also cleanurls

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -4,6 +4,11 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   title: "info.skvidar.run",
   description: "Informasjonsside for Skvidar Lang",
+  cleanUrls: true,
+  ignoreDeadLinks: true,
+  head: [
+    ['script', { defer: true, 'data-domain': 'info.skvidar.run', src: 'https://plausible.tresh.run/js/script.js' }]
+  ],
   srcDir: './src',
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
