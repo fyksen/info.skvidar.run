6e562ed00f33b222a291d4541b375ad0a7ae4064
Fredrik Fyksen <fredrik@fyksen.me>
2023-10-12 09:19:27 +0200

    Corrected wind symbols

--- src/next/WeatherComponent.vue
+++ src/next/WeatherComponent.vue
@@ -64,19 +64,19 @@ export default {
             if (deg >= 337.5 || deg < 22.5) {
                 return '⬆️'; // North
             } else if (deg >= 22.5 && deg < 67.5) {
-                return '↗️'; // North-East
+                return '↖️'; // North-East
             } else if (deg >= 67.5 && deg < 112.5) {
-                return '➡️'; // East
+                return '⬅️'; // East
             } else if (deg >= 112.5 && deg < 157.5) {
-                return '↘️'; // South-East
+                return '↙️'; // South-East
             } else if (deg >= 157.5 && deg < 202.5) {
                 return '⬇️'; // South
             } else if (deg >= 202.5 && deg < 247.5) {
-                return '↙️'; // South-West
+                return '↘️'; // South-West
             } else if (deg >= 247.5 && deg < 292.5) {
-                return '⬅️'; // West
+                return '➡️'; // West
             } else if (deg >= 292.5 && deg < 337.5) {
-                return '↖️'; // North-West
+                return '↗️'; // North-West
             }
         },
         getCloudinessEmoji(cloudAreaFraction) {
